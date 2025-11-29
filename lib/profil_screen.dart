baa47802d7a3baa4f5d278f111c2e1dc0de7d4ed
ulfi <ulfi.aa29@gmail.com>
2025-11-29 23:56:39 +0700

    add profile

---
+++ lib/profil_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class ProfilScreen extends StatelessWidget {
+  const ProfilScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color(0xff38422b),
+        title: const Text(
+          "Profile",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+
+      body: Center(
+        child: ListView(
+          children: [
+            Image.asset("asset/duckling.jpg", height: 100),
+            Center(
+              child: Text(
+                "Nama",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: const Color(0xff38422b),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.favorite),
+                Icon(Icons.favorite),
+                Icon(Icons.favorite),
+              ],
+            ),
+
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Alamat"),
+              subtitle: Text("Midgar"),
+              trailing: Icon(Icons.arrow_circle_right),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.casino_rounded),
+              title: Text("Hobi"),
+              subtitle: Text("Gambling"),
+              trailing: Icon(Icons.arrow_circle_right),
+            ),
+
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.info),
+                title: Text("Logout"),
+                subtitle: Text("Midgar"),
+                trailing: Icon(Icons.exit_to_app),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
